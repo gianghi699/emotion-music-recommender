2bb16caf4ad208792410458bcab187179ffa5a0d
gianghi699 <55063173+gianghi699@users.noreply.github.com>
2025-04-20 20:19:05 -0400

    Update load_model.py

--- app/model/load_model.py
+++ app/model/load_model.py
@@ -1,7 +1,19 @@
+import os
+import gdown
 from tensorflow.keras.models import load_model
-import streamlit as st
 
-@st.cache_resource
-def load_emotion_model(path='app/model/emotion_cnn.h5'):
-    model = load_model(path)
+MODEL_PATH = "app/model/emotion_cnn.h5"
+DRIVE_URL = "https://drive.google.com/uc?id=1rlJq1neQyPk_LkMy5AxKubIZDH-g4dCH
+
+def download_model():
+    if not os.path.exists(MODEL_PATH):
+        print("Model not found locally. Downloading from Google Drive...")
+        gdown.download(DRIVE_URL, MODEL_PATH, quiet=False)
+    else:
+        print("Model already exists. Skipping download.")
+
+def get_emotion_model():
+    download_model()
+    model = load_model(MODEL_PATH)
     return model
+
